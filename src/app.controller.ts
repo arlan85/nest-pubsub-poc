2c67d0437ee30d4afc48d95c375789ae82484716
Arlan Galvez <galvezalonso@gmail.com>
2025-02-13 12:32:00 -0500

    feat: add content realted to basic communocation  pubsub -api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post()
+  async postMessage(@Body() body: any): Promise<any> {
+    return this.appService.Operate(body);
+  }
+  @Post('receive')
+  async receiveMessage(@Body() body: any): Promise<any> {
+    return this.appService.ReceiveNextProcess(body);
   }
 }
